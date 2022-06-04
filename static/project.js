360a1fdb8f3e909f848fb3c16bd993769146ec57
TNice <tylernicewarner@yahoo.com>
2022-06-03 19:16:10 -0500

    removed js that was not needed

--- static/project.js
+++ static/project.js
@@ -1,15 +1,8 @@
 const select = document.getElementById("selection");
 const frame = document.getElementById("frame");
 
-if (select.addEventListener){
-    select.addEventListener('change', (event) => {
-        frame.src = event.target.value;
-        console.log("test");
-    }, false)
-}
-else{
-    select.attachEvent('onchange', (event) => {
-        frame.src = event.target.value;
-        console.log("test");
-    }, false)
-}
+select.addEventListener('change', (event) => {
+    frame.src = event.target.value;
+    console.log("test");
+}, false)
+
